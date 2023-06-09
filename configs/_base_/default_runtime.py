6f69bca9ad4c3a1a8b0d6a7900e4c5807f786270
David Russell <davidrussell327@gmail.com>
2023-06-09 19:03:04 +0000

    Update default runtime

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -4,7 +4,13 @@ env_cfg = dict(
     mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
     dist_cfg=dict(backend='nccl'),
 )
-vis_backends = [dict(type='LocalVisBackend')]
+vis_backends=[dict(type='LocalVisBackend'),
+              dict(type='WandbVisBackend',
+                init_kwargs=dict(
+                entity='safeforest-cmu',
+                project='mmsegmentation_gascola',
+                name='segformer_mit-b0_8xb1-160k_safeforest_gascola_23_04_27_collect_04'))
+              ]
 visualizer = dict(
     type='SegLocalVisualizer', vis_backends=vis_backends, name='visualizer')
 log_processor = dict(by_epoch=False)
